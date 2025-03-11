ed01fa6f7bf806370bba70263a35663d5f50d0e7
annurakeren <annura.rizkya.chantika.tik24@stu.pnj.ac.id>
2025-03-12 01:59:33 +0700

    SKCK.java

--- SKCK.java
+++ SKCK.java
@@ -1,3 +1,116 @@
 public class SKCK {
-  
+  import java.util.ArrayList;
+import java.util.Scanner;
+
+class SKCK {
+    private String nomorSurat;
+    private String nama;
+    private String nik;
+    private String jenisKelamin;
+    private String tempatTanggalLahir;
+    private String agama;
+    private String kewarganegaraan;
+    private String pekerjaan;
+    private String alamat;
+    private String jenisKeperluan;
+    private String namaPemohon;
+    private String tanggal;
+
+    // Getter dan Setter
+    public String getNomorSurat() {
+        return nomorSurat;
+    }
+
+    public void setNomorSurat(String nomorSurat) {
+        this.nomorSurat = nomorSurat;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getNik() {
+        return nik;
+    }
+
+    public void setNik(String nik) {
+        this.nik = nik;
+    }
+
+    public String getJenisKelamin() {
+        return jenisKelamin;
+    }
+
+    public void setJenisKelamin(String jenisKelamin) {
+        this.jenisKelamin = jenisKelamin;
+    }
+
+    public String getTempatTanggalLahir() {
+        return tempatTanggalLahir;
+    }
+
+    public void setTempatTanggalLahir(String tempatTanggalLahir) {
+        this.tempatTanggalLahir = tempatTanggalLahir;
+    }
+
+    public String getAgama() {
+        return agama;
+    }
+
+    public void setAgama(String agama) {
+        this.agama = agama;
+    }
+
+    public String getKewarganegaraan() {
+        return kewarganegaraan;
+    }
+
+    public void setKewarganegaraan(String kewarganegaraan) {
+        this.kewarganegaraan = kewarganegaraan;
+    }
+
+    public String getPekerjaan() {
+        return pekerjaan;
+    }
+
+    public void setPekerjaan(String pekerjaan) {
+        this.pekerjaan = pekerjaan;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    public String getJenisKeperluan() {
+        return jenisKeperluan;
+    }
+
+    public void setJenisKeperluan(String jenisKeperluan) {
+        this.jenisKeperluan = jenisKeperluan;
+    }
+
+    public String getNamaPemohon() {
+        return namaPemohon;
+    }
+
+    public void setNamaPemohon(String namaPemohon) {
+        this.namaPemohon = namaPemohon;
+    }
+
+    public String getTanggal() {
+        return tanggal;
+    }
+
+    public void setTanggal(String tanggal) {
+        this.tanggal = tanggal;
+    }
+}
 }
