be6213950fbd59ebb4de0aeeea3a75fb5d169b5c
lvciddreams <farrelmaahira104@gmail.com>
2025-03-12 00:33:07 +0700

    bikin setter and getter surat keluar dan surat nikah

--- SuratNikah.java
+++ SuratNikah.java
@@ -1,3 +1,86 @@
 public class SuratNikah {
-  
+    private String nomorSurat;
+    private String nama;
+    private String tempatTanggalLahir;
+    private String agama;
+    private int noKTP;
+    private String kewarganegaraan;
+    private String statusPerkawinan;
+    private String pekerjaan;
+    private String alamat;
+    
+    public String getNomorSurat() {
+        return nomorSurat;
+    }
+
+    public void setNomorSurat(String nomorSurat) {
+        this.nomorSurat = nomorSurat;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getTempatTanggalLahir() {
+        return tempatTanggalLahir;
+    }
+
+    public void setTempatTanggalLahir(String tempatTanggalLahir) {
+        this.tempatTanggalLahir = tempatTanggalLahir;
+    }
+
+    public String getAgama() {
+        return agama;
+    }
+
+    public void setAgama(String agama) {
+        this.agama = agama;
+    }
+
+    public int getNoKTP() {
+        return noKTP;
+    }
+    
+    public void setNoKTP(int noKTP) {
+        this.noKTP = noKTP;
+    }
+
+    public String getKewarganegaraan() {
+        return kewarganegaraan;
+    }
+
+    public void setKewarganegaraan(String kewarganegaraan) {
+        this.kewarganegaraan = kewarganegaraan;
+    }
+
+    public String getStatusPerkawinan() {
+        return statusPerkawinan;
+    }
+
+    public void setStatusPerkawinan(String statusPerkawinan) {
+        this.statusPerkawinan = statusPerkawinan;
+    }
+
+    public String getPekerjaan() {
+        return pekerjaan;
+    }
+
+    public void setPekerjaan(String pekerjaan) {
+        this.pekerjaan = pekerjaan;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+    
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    
+
 }
